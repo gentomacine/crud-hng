6b2ff55f51721c7f1031af2fb9550b4496145d46
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2023-09-10 15:56:29 +0100

    adding my svc structure

---
+++ routes/user.route.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const userRoute = express.Router();
+
+//create user
+// userRoute.post("/register", createUser);
+// userRoute.get("/", getUserInfo)
+
+
+export default userRoute;
